022d4592b862eec998df6b399802cba86e401393
Franco Gallina <francoj.gallina@gmail.com>
2023-08-31 21:44:25 -0300

    Borrado de funcion anterior que estaba comentada

--- dags/funciones.py
+++ dags/funciones.py
@@ -9,41 +9,6 @@ from datetime import datetime
 import psycopg2
 
 
-# """----------------------------------EXTRACCIÓN DATOS DE LA API----------------------------------"""
-
-# def get_stock_data(api_key_twelvedata):
-#   list_symbols = ['AAPL', 'MSFT', 'JPM', 'V', 'UNH', 'JNJ', 'WMT', 'HD', 'PG', 'DIS', 'KO', 'CSCO', 'CVX', 'NKE', 'CRM', 'INTC', 'VZ', 'MRK', 'MCD', 'HON', 'BA', 'AMGN', 'AXP', 'GS', 'CAT', 'IBM', 'MMM', 'WBA', 'DOW', 'TRV']
-
-#   URL_API= "https://api.twelvedata.com/time_series"
-
-#   list_prices = []
-
-#   for symbol in list_symbols:
-#     parameters = {
-#       "symbol": symbol,
-#       "interval": "1day",
-#       "format": "JSON",
-#       "start_date": "05/01/2023 6:36 PM",
-#       "end_date": datetime.now().strftime("%m/%d/%Y %I:%M %p"),
-#       "apikey": api_key_twelvedata}
-
-#     response = requests.get(URL_API,parameters)
-#     if response.status_code == 200:
-#       data=response.json()
-#       if "meta" in data:
-#         list_prices.append(data)
-#       else:
-#         time.sleep(10)
-
-#   stock_prices = pd.json_normalize(list_prices, 'values', [['meta','symbol']])
-#   stock_prices = stock_prices.reindex(["meta.symbol", "datetime", "open", "high", "low", "close", "volume"],axis=1)
-#   stock_prices.columns = ["symbol", "date", "open_price", "high_price", "low_price", "close_price", "volume"]
-  
-#   stock_prices.to_csv("stock_prices.csv", index=False)
-#   print("Archivo creado")
-
-
-
 """----------------------------------EXTRACCIÓN DATOS DE LA API----------------------------------"""
 
 def get_stock_data(**context):
